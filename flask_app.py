b670edf8711dbd2f493c994983040a0fd85973dc
funingteng <funingteng@gmail.com>
2017-11-13 13:48:29 -0600

    make it work on pythonanywhere

--- flask_app.py
+++ flask_app.py
@@ -4,18 +4,21 @@ from flask.ext.cors import CORS, cross_origin
 from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
-app.config['CORS_HEADERS'] = 'Content-Type'
-cors = CORS(app, resources={r"/api/dpllsatisfiable": {"origins": "http://localhost:4000"}})
 
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+    
 @app.route('/api/dpllsatisfiable', methods=['POST'])
 @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def dpllsatisfiable():
     if not request.json or not 'knowledgebase' in request.json:
         abort(400)
+    
     knowledgebase = request.json['knowledgebase']    
     result = str(dpll_satisfiable(expr(knowledgebase)))
     return jsonify({'result':result}), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
